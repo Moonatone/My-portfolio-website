84ecac06dedf25c4fa801147d69f90a4628f9193
Nick <nickhuang0610@gmail.com>
2026-09-01 22:54:52 +0200

    lol

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,29 @@
-import { useEffect } from 'react'
-import { Routes, Route, useLocation } from 'react-router-dom'
-import { useSelector } from 'react-redux'
-import Navbar from './components/Navbar'
-import Footer from './components/Footer'
-import ProjectModal from './components/ProjectModal'
-import Toast from './components/Toast'
-import Home from './pages/Home'
-import Projects from './pages/Projects'
-import About from './pages/About'
-import Contact from './pages/Contact'
+import { useEffect } from "react";
+import { Routes, Route, useLocation } from "react-router-dom";
+import { useSelector } from "react-redux";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import ProjectModal from "./components/ProjectModal";
+import Toast from "./components/Toast";
+import Home from "./pages/Home";
+import Projects from "./pages/Projects";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
 
 function ScrollToTop() {
-  const { pathname } = useLocation()
+  const { pathname } = useLocation();
   useEffect(() => {
-    window.scrollTo(0, 0)
-  }, [pathname])
-  return null
+    window.scrollTo(0, 0);
+  }, [pathname]);
+  return null;
 }
 
 export default function App() {
-  const mode = useSelector((s) => s.theme.mode)
+  const mode = useSelector((s) => s.theme.mode);
 
   useEffect(() => {
-    document.documentElement.classList.toggle('dark', mode === 'dark')
-  }, [mode])
+    document.documentElement.classList.toggle("dark", mode === "dark");
+  }, [mode]);
 
   return (
     <div className="flex min-h-screen flex-col">
@@ -35,11 +35,12 @@ export default function App() {
           <Route path="/projects" element={<Projects />} />
           <Route path="/about" element={<About />} />
           <Route path="/contact" element={<Contact />} />
+          <Route path="/My-portfolio-website" element={<Home />} />
         </Routes>
       </main>
       <Footer />
       <ProjectModal />
       <Toast />
     </div>
-  )
+  );
 }
